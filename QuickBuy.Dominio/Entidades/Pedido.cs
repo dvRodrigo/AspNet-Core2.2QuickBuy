33cc6ead6d96926e07c1f559ff16cb3128ba4fda
dvRodrigo <rodrigovieirablack@gmail.com>
2020-11-19 20:07:29 -0300

    Implementando classe Entidade e validacoes

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -16,9 +17,20 @@ namespace QuickBuy.Dominio.Entidades
         public string EnderecoComplemento { get; set; }
         public int NumeroEndereco { get; set; }
         public int FormaPagamentoId { get; set; }
-      public FormaPagamento FormaPagamento { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
         public ICollection<ItemPedido> ItensPedido { get; set; }
-      
 
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any())
+            {
+                AdicionarCritica("Pedido não pode ficar sem item de pedido!!");
+            }
+            if (string.IsNullOrEmpty(CEP))
+            {
+                AdicionarCritica("CEP  deve estar preenchido!!");
+            }
+        }
     }
 }
